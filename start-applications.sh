189fda628a77fa09b1b4a785b0777d96fa86224f
Mark Clarke <mark.clarke@reedonline.co.uk>
2019-12-09 10:12:49 +0000

    Add script to start applications on boot up

---
+++ start-applications.sh
@@ -0,0 +1,11 @@
+# Launch websites
+open -a "Google Chrome.app"
+open -a "Firefox.app"
+
+# Launch Applications
+open -a "Slack.app"
+open -a "Visual Studio Code.app"
+
+# Upgrade applications
+brew upgrade
+brew cask upgrade
