9613f4a9d2166e92e0b335a134ea4c25aaca19c0
Mark Clarke <mark.clarke@reedonline.co.uk>
2019-12-11 10:48:35 +0000

    Update user messages

--- start-applications.sh
+++ start-applications.sh
@@ -6,28 +6,19 @@ SECONDS=0
 
 # Launch websites
 echo ""
-echo "Launching Chrome ... "
+echo "Launching Chrome, FireFox, Slack, Visual Studio Code ... "
 open -a "Google Chrome.app"
-
-echo "Launching Firefox ... "
 open -a "Firefox.app"
 
 # Launch Applications
-echo "Launching Slack ... "
 open -a "Slack.app"
-
-echo "Launching VS Code ... "
 open -a "Visual Studio Code.app"
 
 # Upgrade applications
-echo ""
-echo "Updating brew packages ... "
 brew upgrade
-
-echo "Updating brew casks ... "
 brew cask upgrade
 
 
 echo ""
-echo "Starup tasks finished in $(printf '%02dh:%02dm:%02ds\n' $(($SECONDS/3600)) $(($SECONDS%3600/60)) $(($SECONDS%60)))"
+echo "Starup tasks finished in $(printf '%02dm:%02ds\n' $(($SECONDS%3600/60)) $(($SECONDS%60)))"
 echo ""
